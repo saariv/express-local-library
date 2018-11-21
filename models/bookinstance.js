b55439c632a0b612131f4d9137e8e06f44d3bd91
saariv <44223117+saariv@users.noreply.github.com>
2018-11-21 18:36:48 +0200

    Add files via upload

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,3 +1,5 @@
+var moment = require('moment');
+
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
@@ -16,6 +18,12 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 //Export model
